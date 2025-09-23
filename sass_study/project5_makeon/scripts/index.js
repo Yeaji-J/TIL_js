be2bdb03f46909c8d1bab4d3806a4c4d43aeabd7
jjuglish <jjuglish@gmail.com>
2025-09-23 18:14:18 +0900

    메이크온 상품페이지 JS 1번 완성

--- sass_study/project5_makeon/scripts/index.js
+++ sass_study/project5_makeon/scripts/index.js
@@ -12,7 +12,7 @@
 // 12. 위 팝업창에서 확인 누르면 로그인 페이지로 이동, 로그인 상태면 각각 페이지로 이동
 
 // 1. 작은 이미지 마우스 올리면 opacity 낮아짐, 마우스 치우면 돌아옴
-const smallImage = document.querySelectorAll('.small_images')
+const smallImage = document.querySelectorAll('.small_images') //a 태그
 function imgMouseFunc (img){
     img.addEventListener('mouseover',()=>{
         img.style.opacity = '50%'
@@ -24,15 +24,59 @@ function imgMouseFunc (img){
 for(let i=0; i<smallImage.length; i++){imgMouseFunc(smallImage[i]);}
 
 // 2. 작은 이미지 클릭 시, 검은색 테두리 & 큰 이미지 슬라이드 & 아래 페이지네이션 움직이기
+const bigImage = document.querySelector('.big_image_group') // img 태그, 슬라이드
+const pagenationA = document.querySelectorAll('.pagenation a')
 
-function imgClickFunc (img){
-    img.style.border = '1px solid #111'
-    
-}
-smallImage[0].addEventListener('click',()=>{
-    smallImage[0].style.border = '1px solid #111'
-})
-smallImage[1].addEventListener('click',()=>{
+let imgSlideWidth = bigImage.children[0].offsetWidth;
+bigImage.style.transition = 'transform 0.3s'
+
+/* smallImage[1].addEventListener('click',(e)=>{
+    e.preventDefault();
+    //테두리
     smallImage[1].style.border = '1px solid #111'
     smallImage[0].style.border = ''
+    //큰 이미지 슬라이드
+    bigImage.style.transform = `translateX(-${imgSlideWidth*1}px)`
+    //페이지네이션 움직이기
+    pagenationA[1].classList.add('active')
+    pagenationA[0].classList.remove('active')
+}) */
+for(let i of smallImage)
+for(let i=0; i<smallImage.length; i++){
+    smallImage[i].addEventListener('click',(e)=>{
+        e.preventDefault();
+        //테두리
+        smallImage[i].style.border = '1px solid #111'
+        //큰 이미지 슬라이드
+        bigImage.style.transform = `translateX(-${imgSlideWidth*i}px)`
+        //페이지네이션 움직이기
+        pagenationA[i].classList.add('active')
+    })
+}
+// 3. 이미지 상하 이동 버튼 클릭 시 슬라이드 (사진 더 추가되면)
+
+// 4. 공유하기 버튼에 마우스 올리면 투명해지고, 버튼 클릭하면 공유하기 팝업
+const shareIcon = document.querySelector('.share_icon')
+shareIcon.addEventListener('mouseover',()=>{
+    
+})
+function imgMouseFunc (img){
+    img.addEventListener('mouseover',()=>{
+        img.style.opacity = '50%'
+    })
+    img.addEventListener('mouseout',()=>{
+        img.style.opacity = '100%'
+    })
+}
+for(let i=0; i<smallImage.length; i++){imgMouseFunc(smallImage[i]);}
+
+
+// 10. 위시 버튼 클릭 시 하트 검은색으로 바뀜
+const wishBtn = document.querySelector('#wish_btn')
+const wishSrc = wishBtn.children[0].src
+console.log(wishSrc)
+wishBtn.addEventListener('click',()=>{
+    if(wishSrc == "./images/wish_off_icon.png"){
+        wishSrc = "./images/wish_on_icon.png"
+    }else{wishSrc = "./images/wish_off_icon.png"}
 })
